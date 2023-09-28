abe3512afb52c6fba2820643bc89449d969667d8
John komba <john.mwangi1@student.moringaschool.com>
2023-09-28 17:32:26 +0300

    done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const taskDescription = document.getElementById('new-task-description').value;
+
+    if (taskDescription) {
+      const taskItem = document.createElement('li');
+      taskItem.textContent = taskDescription;
+
+      taskList.appendChild(taskItem);
+
+      document.getElementById('new-task-description').value = '';
+    }
+  });
 });
